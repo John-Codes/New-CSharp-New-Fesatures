688da7fa2208b2b5d6655b63bfe79e52d584db8e
efexzium <efexzium@gmail.com>
2023-01-23 17:23:10 -0500

    Add project files.

---
+++ New CSharp Fesatures/Program.cs
@@ -0,0 +1,72 @@
+﻿//Generic Math Support: This feature allows you to perform mathematical operations on generic types, like when you're trying to figure out how much pizza to order for a party.
+
+var count = Math.Max(10, 20);
+//Joke: "Why did the math book look sad? Because it had too many problems"
+//Explanation: With this new feature you can use mathematical operations on different types, like int, float, double etc.
+
+//Numeric IntPtr and UIntPtr: These new types allow you to work with pointer-sized integers, like when you're trying to remember someone's phone number but it's too big to fit in your head.
+
+IntPtr num = (IntPtr)1000;
+
+//Joke: "Why did the computer go to the doctor? it had a pointer problem"
+//Explanation: These new types allow you to work with large integers that are the size of a pointer, which could be useful for memory-intensive applications.
+
+
+//Auto - default struct: This feature allows you to create structs with default values without having to explicitly specify them, like when you're ordering a sandwich and you don't have to specify all the toppings because they come with it by default.
+class test1
+{
+    public test1()
+    {
+        var sandwich = new Sandwich(); // sandwich.Bread and sandwich.Mayonnaise are both set to their default values.
+
+    }
+    struct Sandwich { public int Bread; public bool Mayonnaise; }
+
+}
+//Joke: "Why did the sandwich go to the doctor? It was feeling bread."
+//Explanation: With this new feature you can create struct with default values without having to specify them.
+
+
+
+
+//Pattern match Span<char> or ReadOnlySpan<char> on a constant string: This feature allows you to perform pattern matching on strings, like when you're trying to find a specific song in a playlist.
+
+
+class Test2
+{
+    public Test2()
+    {
+
+        string song = "song1";
+        if (song is var s && s.Length > 0)
+            Console.WriteLine($"The song {s} has a length greater than 0.");
+    }
+}
+//Joke: "Why did the string go to the gym? to get fit."
+//Explanation: With this new feature you can perform pattern matching on strings, you can check if the string matches a certain pattern and perform an action based on that.
+
+
+//Extended nameof scope: This feature allows you to use the nameof operator to refer to members of a class, like when you're trying to remember the name of your friend's dog.
+
+
+class Test3
+{
+    public Test3()
+    {
+        var dog = new Dog { Name = "Fido" };
+        Console.WriteLine(nameof(dog.Name)); // "Name"
+
+
+    }
+    class Dog
+    {
+        public string Name { get; set; }
+    }
+}
+
+
+//Joke: "Why did the dog go to the class? to learn a new name."
+//Explanation: With this new feature you can use the nameof operator to refer to the members of a class, this could be useful for debugging and error handling.
+
+//I hope these examples help illustrate the new features in a relatable way, and also the jokes helped to lighten up the mood. 
+
